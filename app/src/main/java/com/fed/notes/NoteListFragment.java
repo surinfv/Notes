f92a55936ec12a7c4f9d2f45a279b3fda1701e18
surinfv <surinfv@gmail.com>
2017-05-10 13:01:27 +0300

    recycler created

--- app/src/main/java/com/fed/notes/NoteListFragment.java
+++ app/src/main/java/com/fed/notes/NoteListFragment.java
@@ -1,11 +1,29 @@
 package com.fed.notes;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by f on 10.05.2017.
  */
 
 public class NoteListFragment extends Fragment {
+    private RecyclerView mNoteRecyclerView;
 
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_note_list, container, false);
+
+        mNoteRecyclerView = (RecyclerView) view.findViewById(R.id.note_recycler_view);
+        mNoteRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        return view;
+    }
 }
