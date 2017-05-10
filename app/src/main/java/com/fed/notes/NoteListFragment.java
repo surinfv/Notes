179527b4c3d3e29771a2786cbce13f68f45b462a
surinfv <surinfv@gmail.com>
2017-05-10 13:38:37 +0300

    simple recycler added

--- app/src/main/java/com/fed/notes/NoteListFragment.java
+++ app/src/main/java/com/fed/notes/NoteListFragment.java
@@ -8,6 +8,9 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
 
 /**
  * Created by f on 10.05.2017.
@@ -15,6 +18,7 @@ import android.view.ViewGroup;
 
 public class NoteListFragment extends Fragment {
     private RecyclerView mNoteRecyclerView;
+    private NoteAdapter mAdapter;
 
     @Nullable
     @Override
@@ -24,6 +28,52 @@ public class NoteListFragment extends Fragment {
         mNoteRecyclerView = (RecyclerView) view.findViewById(R.id.note_recycler_view);
         mNoteRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
+
+        updateUI();
         return view;
+    }
+
+    private class NoteHolder extends RecyclerView.ViewHolder {
+
+        public TextView mTitleTextView;
+
+        public NoteHolder(View itemView) {
+            super(itemView);
+
+            mTitleTextView = (TextView) itemView;
+        }
+    }
+
+    private class NoteAdapter extends RecyclerView.Adapter<NoteHolder> {
+        private List<Note> mNotes;
+
+        public NoteAdapter(List<Note> notes) {
+            mNotes = notes;
+        }
+
+        @Override
+        public NoteHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(getActivity());
+            View view = inflater.inflate(android.R.layout.simple_list_item_1, parent, false);
+            return new NoteHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(NoteHolder holder, int position) {
+            Note note = mNotes.get(position);
+            holder.mTitleTextView.setText(note.getTitle());
+        }
+
+        @Override
+        public int getItemCount() {
+            return mNotes.size();
+        }
+    }
+
+    private void updateUI() {
+        NoteBook notebook = NoteBook.get(getActivity());
+        List<Note> notes = notebook.getNotes();
+        mAdapter = new NoteAdapter(notes);
+        mNoteRecyclerView.setAdapter(mAdapter);
     }
 }
