4d0c15fcb146236a34acb130b1f246c74111479d
surinfv <surinfv@gmail.com>
2017-05-12 13:48:37 +0300

    too appCaompatActivity

--- app/src/main/java/com/fed/notes/SingleFragmentActivity.java
+++ app/src/main/java/com/fed/notes/SingleFragmentActivity.java
@@ -2,14 +2,14 @@ package com.fed.notes;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by f on 10.05.2017.
  */
 
-public abstract class SingleFragmentActivity extends FragmentActivity {
+public abstract class SingleFragmentActivity extends AppCompatActivity {
 
     protected abstract Fragment createFragment();
 
